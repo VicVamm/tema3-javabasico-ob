8926edabc9ca6649d3d3d83fb9d1d854e2b3a9d2
VicVamm <119972463+VicVamm@users.noreply.github.com>
2023-01-31 14:21:18 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String[] nombres = {"Victor", "Sixto", "Jairo", "Rafael",};
+
+        for (int i = 0; i < nombres.length; i++) {
+            System.out.println(nombres[i]);
+        }
+    }
+}
