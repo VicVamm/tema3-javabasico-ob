44e33e3cd3392716bd0d3fcaa0a20ef5f596099f
VicVamm <119972463+VicVamm@users.noreply.github.com>
2023-01-31 14:23:52 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        String[] nombres = {"Victor", "Sixto", "Jairo", "Rafael",};
+        String[] nombres = {"Victor ", "Sixto ", "Jairo ", "Rafael ",};
 
         for (int i = 0; i < nombres.length; i++) {
             System.out.print(nombres[i]);
